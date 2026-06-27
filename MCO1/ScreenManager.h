153ecdadbb51adcefe3d0a38e9529108035068d9
VPangan <vince_pangan@dlsu.edu.ph>
2026-06-27 11:47:45 +0800

    Updated ScreenManager.h
    Changed openScreen() signature to take Process*, added getTimestamp() public, added reportUtil(), added private runScreenLoop().

--- MCO1/ScreenManager.h
+++ MCO1/ScreenManager.h
@@ -10,14 +10,25 @@ public:
     static void initialize();
     static void destroy();
 
-    void openScreen(const String& processName);
+    // screen -s  : open interactive view for a newly created process
+    void openScreen(Process* process);
+
+    // screen -r  : reattach to an existing process by name
     void reattachScreen(const String& processName);
+
+    // screen -ls : list all running and finished processes
     void listScreens();
+
+    // report-util: write csopesy-log.txt
+    void reportUtil();
+
+    // Utility: get a formatted timestamp string (used by ConsoleManager too)
+    String getTimestamp();
 
 private:
     ScreenManager() {}
     static ScreenManager* instance;
 
     void printProcessSMI(Process* process);
-    String getCurrentTimestamp();
+    void runScreenLoop(Process* process);
 };
