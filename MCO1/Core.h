087ba7d01963d9641fc409c28e8a7f9d3ba6535b
VPangan <vince_pangan@dlsu.edu.ph>
2026-06-27 11:42:41 +0800

    Updated Core.h
    Added quantum param to assignProcess(), added onPreempt callback, added quantumSlice private member.

--- MCO1/Core.h
+++ MCO1/Core.h
@@ -2,17 +2,26 @@
 #include "IETThread.h"
 #include "Process.h"
 #include <atomic>
+#include <functional>
 
 class Core : public IETThread {
 public:
     Core(int id);
     void run() override;
-    void assignProcess(Process* process);
+
+    // Assign a process to run for exactly `quantum` instructions (-1 = run to completion, i.e. FCFS)
+    void assignProcess(Process* process, int quantum = -1);
+
     bool isAvailable();
     int getId();
+
+    // Called by ProcessScheduler to give back a preempted process
+    // Scheduler sets this callback so Core can hand the process back
+    std::function<void(Process*)> onPreempt;
 
 private:
     int id;
     Process* currentProcess;
     std::atomic<bool> available;
+    int quantumSlice;   // instructions to execute this turn (-1 = unlimited)
 };
